cbf059002b6b8179517d41e9c1c86baeaf11e5c7
Juan Guerrero <juan.guerrero.lozano@gmail.com>
2012-07-31 19:34:36 -0400

    Add rule match count to console output

--- rulechecker.js
+++ rulechecker.js
@@ -1,6 +1,22 @@
 (function(){
-  
-  for ( index in document.styleSheets){
-    console.info("Stylesheet " + index + ": " + document.styleSheets[index].href);
+  var stylesheets = document.styleSheets;  
+  for ( index in stylesheets){
+    var currentStylesheet = stylesheets[index];
+    if ( currentStylesheet != null){
+      console.info("Stylesheet " + index + ": " + currentStylesheet.href);
+      var rules = currentStylesheet.rules;
+      for ( j in rules){
+        var currentRule = rules[j];
+        var selectorText = currentRule.selectorText;
+        console.info(selectorText);
+        var matchedElements = $(selectorText);
+        if ( matchedElements.length() > 0 ){
+          console.info(selectorText + " applies to " + matchedElements.length() + "elements");
+        } else {
+          console.warn(selectorText + " doesn't apply");
+        }
+      } 
+    }
+    
   }
 })();
