64e1e6ec6bffc29a27c995d9fc5ec464b3ec83b5
Juan Guerrero <juan.guerrero.lozano@gmail.com>
2012-07-31 20:01:11 -0400

    Add summary log

--- rulechecker.js
+++ rulechecker.js
@@ -1,24 +1,30 @@
 (function(){
   
   var stylesheets = document.styleSheets;  
+  var totalRuleCount = 0;
+  var totalAppliedRuleCount = 0;
   for ( index in stylesheets){
     var currentStylesheet = stylesheets[index];
     if ( currentStylesheet != null){
       console.info("Stylesheet " + index + ": " + currentStylesheet.href);
       var rules = currentStylesheet.rules;
       for ( j in rules){
+        totalRuleCount += 1;
         var currentRule = rules[j];
         var selectorText = currentRule.selectorText;
-        console.info(selectorText);
         var matchedElements = $(selectorText);
         
         if ( matchedElements.length > 0 ){
+          totalAppliedRuleCount += 1;
           console.info(selectorText + " applies to " + matchedElements.length + "elements");
         } else {
           console.warn(selectorText + " doesn't apply");
         }
       } 
     }
+    
+    console.info("There are " + totalRuleCount + " rules");
+    console.info("Applied: " + totalAppliedRuleCount + ", Not applied: " + totalRuleCount - totalAppliedRuleCount);
     
   }
 })();
