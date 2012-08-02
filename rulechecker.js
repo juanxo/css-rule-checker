3cd94a7f9e95de0c283564f94840083b72efba3e
Juan Guerrero <juan.guerrero.lozano@gmail.com>
2012-08-02 11:04:00 +0200

    Add autoloading of jquery if it isn't loaded

--- rulechecker.js
+++ rulechecker.js
@@ -1,32 +1,42 @@
 (function(){
-  
-  var stylesheets = document.styleSheets;  
-  var totalRuleCount = 0;
-  var totalAppliedRuleCount = 0;
-  for ( index in stylesheets){
-    var currentStylesheet = stylesheets[index];
-    if ( currentStylesheet != null){
-      console.info("Stylesheet " + index + ": " + currentStylesheet.href);
-      var rules = currentStylesheet.rules;
-      for ( j in rules){
-        totalRuleCount += 1;
-        var currentRule = rules[j];
-        var selectorText = currentRule.selectorText;
-        var matchedElements = $(selectorText);
-        
-        if ( matchedElements.length > 0 ){
-          totalAppliedRuleCount += 1;
-          console.info(selectorText + " applies to " + matchedElements.length + "elements");
-        } else {
-          console.warn(selectorText + " doesn't apply");
-        }
-      } 
-    }
-    
-    
+  if (!($ = window.jQuery)) { 
+    script = document.createElement( 'script' );  
+    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';   
+    script.onload=checkRules;  
+    document.body.appendChild(script);  
+  }   
+  else {  
+      checkRules();  
   }
-  console.info("There are " + totalRuleCount + " rules");
-  var notAppliedRuleCount = totalRuleCount - totalAppliedRuleCount;
-  console.info("Applied: " + totalAppliedRuleCount + ", Not applied: " + notAppliedRuleCount);
-    
+  
+  function checkRules() {
+    var stylesheets = document.styleSheets;  
+    var totalRuleCount = 0;
+    var totalAppliedRuleCount = 0;
+    for ( index in stylesheets){
+      var currentStylesheet = stylesheets[index];
+      if ( currentStylesheet != null){
+        console.info("Stylesheet " + index + ": " + currentStylesheet.href);
+        var rules = currentStylesheet.rules;
+        for ( j in rules){
+          totalRuleCount += 1;
+          var currentRule = rules[j];
+          var selectorText = currentRule.selectorText;
+          var matchedElements = $(selectorText);
+          
+          if ( matchedElements.length > 0 ){
+            totalAppliedRuleCount += 1;
+            console.info(selectorText + " applies to " + matchedElements.length + "elements");
+          } else {
+            console.warn(selectorText + " doesn't apply");
+          }
+        } 
+      }
+      
+      
+    }
+    console.info("There are " + totalRuleCount + " rules");
+    var notAppliedRuleCount = totalRuleCount - totalAppliedRuleCount;
+    console.info("Applied: " + totalAppliedRuleCount + ", Not applied: " + notAppliedRuleCount);
+  }    
 })();
